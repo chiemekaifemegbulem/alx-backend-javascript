103fb4c338f586fc6304f6ffbed4a415afca9a5e
chiemekaifemegbulem <samuelifemegbulem@my.uopeople.edu>
2023-10-03 21:52:53 +0000

    a function that returns a set from array

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Chiemeka Ifemegbulem <https://github.com/chiemekaifemegbulem>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
