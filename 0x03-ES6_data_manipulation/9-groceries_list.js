89fef9a85b9415697d299a16c3db199bdd18e53f
chiemekaifemegbulem <samuelifemegbulem@my.uopeople.edu>
2023-10-03 22:05:43 +0000

    a function that returns map of groceries

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,15 @@
+/**
+ * Retrieves a map of grocery names and their quantity.
+ * @author Chiemeka Ifemegbulem <https://github.com/chiemekaifemegbulem>
+ * @returns {Map<string, Number>}
+ */
+export default function groceriesList() {
+  const values = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+  return new Map(values);
+}
