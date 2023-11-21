3c6aba5a9431a780c6de592ee0c6be87e1f3bde2
chiemekaifemegbulem <samuelifemegbulem@my.uopeople.edu>
2023-11-21 14:34:57 +0000

    updated

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,7 @@
 /**
  * Displays a message on the STDOUT.
  * @param {String} msg The message to display.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Chiemeka Ifemegbulem <https://github.com/chiemekaifemegbulem>
  */
 const displayMessage = (msg) => {
   console.log(msg);
