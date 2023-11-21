c2e15dc34c37be047a96d6b64c8bd6d0ce8dde7d
chiemekaifemegbulem <samuelifemegbulem@my.uopeople.edu>
2023-11-21 14:56:14 +0000

    updated

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -9,7 +9,7 @@ const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
 /**
  * Counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Chiemeka Ifemegbulem <https://github.com/chiemekaifemegbulem>
  */
 const countStudents = (dataPath) => new Promise((resolve, reject) => {
   if (!dataPath) {
